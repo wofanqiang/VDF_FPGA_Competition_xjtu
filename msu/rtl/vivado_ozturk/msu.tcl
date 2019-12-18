3d7bf1aec95e123ff64f11e7248cd561e97c8f3c
wofanqiang <lbking89@163.com>
2019-12-18 08:56:08 +0800

    fix

--- msu/rtl/vivado_ozturk/msu.tcl
+++ msu/rtl/vivado_ozturk/msu.tcl
@@ -213,6 +213,7 @@ set files [list \
  [file normalize "${origin_dir}/../sdaccel/vdf_control_s_axi.v"] \
  [file normalize "${origin_dir}/../../../modular_square/rtl/modular_square_2_cycle.sv"] \
  [file normalize "${origin_dir}/../../../modular_square/rtl/xpb_lut.sv"] \
+ [file normalize "${origin_dir}/../../../modular_square/rtl/cdc_sync_valid.sv"] \
  [file normalize "${origin_dir}/msu.srcs/msuconfig.vh"] \
  [file normalize "${origin_dir}/../msu.sv"] \
  [file normalize "${origin_dir}/../msu_cdc.sv"] \
@@ -241,6 +242,12 @@ set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
 
 set file "$origin_dir/../../../modular_square/rtl/xpb_lut.sv"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
+
+
+set file "$origin_dir/../../../modular_square/rtl/cdc_sync_valid.sv"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
