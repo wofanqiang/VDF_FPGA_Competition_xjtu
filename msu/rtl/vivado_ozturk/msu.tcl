2afe85fed6835d0ed742cbffd3d83df5aa7cd2c6
wofanqiang <lbking89@163.com>
2019-12-18 18:17:19 +0800

    fix

--- msu/rtl/vivado_ozturk/msu.tcl
+++ msu/rtl/vivado_ozturk/msu.tcl
@@ -226,7 +226,7 @@ add_files -norecurse -fileset $obj $files
 
 # Set 'sources_1' fileset file properties for remote files
 
-set file "$origin_dir/../../../modular_square/rtl/modular_square_8_cycles.sv"
+set file "$origin_dir/../../../modular_square/rtl/modular_square_2_cycle.sv"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
