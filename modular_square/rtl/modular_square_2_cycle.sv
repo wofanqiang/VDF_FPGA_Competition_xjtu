44825c19889548747e66b90056ef8dc4dd84b75e
wofanqiang <lbking89@163.com>
2019-12-20 14:26:01 +0800

    fix

--- modular_square/rtl/modular_square_2_cycle.sv
+++ modular_square/rtl/modular_square_2_cycle.sv
@@ -38,6 +38,8 @@ module modular_square_2_cycle
     logic [BIT_LEN-1:0] u_ms[NUM_ELEMENTS];
     (* max_fanout = 20 *) logic [BIT_LEN-1:0] sq_out_reg[NUM_ELEMENTS];
 
+    assign sq_out = sq_out_reg;
+
     //(* max_fanout = 20 *) logic [9:0] state_current;
     //(* max_fanout = 20 *) logic [9:0] state_next;
 
